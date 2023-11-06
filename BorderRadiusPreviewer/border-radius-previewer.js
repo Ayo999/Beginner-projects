5b0591940e11ba1cb4b1e29fd68a71194d47e7ec
Ayo999 <sulaimanbasit05@gmail.com>
2023-11-06 17:40:23 +0100

    Modified border-radius-previewer.html and border-radius-previewer.js

--- BorderRadiusPreviewer/border-radius-previewer.js
+++ BorderRadiusPreviewer/border-radius-previewer.js
@@ -1,20 +1,17 @@
-const innerSquare = document.querySelector("[data-inner-square]")
 const outerSquare = document.querySelector("[data-outer-square]")
 const inputs = document.querySelectorAll("[data-border-radius]")
-const outputs = document.querySelectorAll("[data-output]")
 
-inputs.forEach(input => {
-    input.addEventListener('input', (e) => {
-        const borderRadius = input.dataset.borderRadius
-
-        if(borderRadius == 'top-left-border-radius'){
-            outerSquare.style.borderTopLeftRadius = `${input.value}px`
-        }else if(borderRadius == 'top-right-border-radius'){
-            outerSquare.style.borderTopRightRadius = `${input.value}px`
-        }else if(borderRadius == 'bottom-right-border-radius'){
-            outerSquare.style.borderBottomRightRadius = `${input.value}px`
-        }else outerSquare.style.borderBottomLeftRadius = `${input.value}px`
-    })
-    }) 
+inputs.forEach((input) => 
+  input.addEventListener("input", (e) => {
+    const field = input.dataset.borderRadius;
+    let output = document.getElementById(`${field}Output`)
+    outerSquare.style[field] = `${input.value}px`;
+    if(input.value == 0){
+        output.textContent = ''
+    }else{
+        output.textContent = `${field}: ${input.value}px;`;
+    }
+  })
+);
 
     
